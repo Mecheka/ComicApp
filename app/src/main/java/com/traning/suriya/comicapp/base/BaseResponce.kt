1ef14e3f34593a5d30592105502694adf96234f8
Suriya <mcmio125@gmail.com>
2019-03-28 14:49:24 +0700

    add base responce and viewmode

--- app/src/main/java/com/traning/suriya/comicapp/base/BaseResponce.kt
+++ app/src/main/java/com/traning/suriya/comicapp/base/BaseResponce.kt
@@ -1,2 +1,9 @@
 package com.traning.suriya.comicapp.base
 
+import com.google.gson.annotations.SerializedName
+
+open class BaseResponce {
+    @SerializedName("status")
+    var status: Int? = null
+}
+
